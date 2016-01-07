abe86569ad1e980aa0513980c5e5f2bf1c697728
samuelohterion <samuel.ohterion@gmx.de>
2016-01-07 02:33:39 +0100

    just for being up to date

--- projects/T20151229/main.cpp
+++ projects/T20151229/main.cpp
@@ -1,5 +1,6 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 #include <vector>
 #include "tensor.hpp"
 
@@ -13,7 +14,8 @@ main( ) {
 	EIdx
 	i,
 	j,
-	k;
+	k,
+	l;
 
 
 	EIdx
@@ -32,6 +34,7 @@ main( ) {
 	s.addIdx( &k );
 	s.addIdx( &ci );
 	s.addIdx( &k );
+	s.addIdx( &l );
 
 	Counter
 	c;
@@ -73,13 +76,23 @@ main( ) {
 
 	c.reset( );
 
-	cout << setw( 8 ) << "i + j + k" << endl;
+	cout << endl << setw( 8 ) << "100 · i + 10 · j + k" << endl;
 
 	while( c.isOK( ) ) {
 
 		cout << setw( 8 ) << ( 100 * i + 10 * j + k ).str( ) << endl;
 
 		++c;
+	}
+
+	cout << endl << setw( 8 ) << "1000 · i + 100 · j + 10 · k + l" << endl;
+
+	int
+	cnt = 2;
+
+	for( i.setFirst( 1 ).setCount( ++cnt ), j.setFirst( 1 ).setCount( ++cnt ), k.setFirst( 1 ).setCount( ++cnt ), l.setFirst( 1 ).setCount( ++cnt ), c.reset( ); c.isOK( ); ++c ) {
+
+		cout << ( c.size( ) - c.lcd( ) > 0 ? string( c.size( ) - c.lcd( ) - 1u, '\n' ) : "" ) << setw( 6 ) << ( 1000 * i + 100 * j + 10 * k + l ).str( );
 	}
 
 	cout << endl;
